a01a7d295c0172141825d19765cad02bd4a1fd31
iaa2005 <marsee3015@gmail.com>
2021-09-11 22:12:46 +0300

    -

--- index.js
+++ index.js
@@ -28,18 +28,18 @@ value_locked.innerText = "$" + String(0);
 nodes_count.innerText = String(0);
 
 
-(function(){
-    let w = document.documentElement.clientWidth,
-        h = document.documentElement.clientHeight;
+// (function(){
+//     let w = document.documentElement.clientWidth,
+//         h = document.documentElement.clientHeight;
 
-    document.body.addEventListener('mousemove', function(evt){
-        let posX = Math.round(evt.clientX / w * 25 + 100)
-        let posY = Math.round(evt.clientY / h * 25 + 140)
-        document.getElementById('logo-image-div1').style.marginTop = String(posY)+'px'
-        document.getElementById('logo-image-div1').style.marginLeft = String(posX)+'px'
-        posX = Math.round(evt.clientX / w * 50 + 170)
-        posY = Math.round(evt.clientY / h * 50 + 120)
-        document.getElementById('logo-image-div2').style.marginTop = String(posY)+'px'
-        document.getElementById('logo-image-div2').style.marginLeft = String(posX)+'px'
-    })
-})()
+//     document.body.addEventListener('mousemove', function(evt){
+//         let posX = Math.round(evt.clientX / w * 25 + 100)
+//         let posY = Math.round(evt.clientY / h * 25 + 140)
+//         document.getElementById('logo-image-div1').style.marginTop = String(posY)+'px'
+//         document.getElementById('logo-image-div1').style.marginLeft = String(posX)+'px'
+//         posX = Math.round(evt.clientX / w * 50 + 170)
+//         posY = Math.round(evt.clientY / h * 50 + 120)
+//         document.getElementById('logo-image-div2').style.marginTop = String(posY)+'px'
+//         document.getElementById('logo-image-div2').style.marginLeft = String(posX)+'px'
+//     })
+// })()
